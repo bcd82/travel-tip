f455dc761c8a142572a3240261ee9dddcae31711
ronbuchris <ronbuchris@gmail.com>
2021-08-18 14:31:13 +0300

    storage-service

---
+++ js/services/storage-service.js
@@ -0,0 +1,18 @@
+export const storageService = {
+    load: loadFromStorage,
+    save: saveToStorage
+}
+
+
+
+
+function loadFromStorage(key) {
+    var json = localStorage.getItem(key)
+    var data = JSON.parse(json)
+    return data;
+}
+
+function saveToStorage(key, data) {
+    var json = JSON.stringify(data);
+    localStorage.setItem(key, json)
+}
