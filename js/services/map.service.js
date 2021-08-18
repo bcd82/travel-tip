f6e81ae9669d0bca99603e87ca663e7becd0c58d
bcd82 <barak.sidi@gmail.com>
2021-08-19 00:22:59 +0300

    fixed weather api ?

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -1,93 +1,98 @@
-import { locService } from './loc.service.js';
-import { storageService } from './storage.service.js';
+import {
+    locService
+} from './loc.service.js'
+import {
+    storageService
+} from './storage.service.js';
 
 const API_KEY = 'AIzaSyCFyoGS4I6uoOKNtMDd5nLMcv-n8jECKFQ'; //TODO: Enter your API Key
-const KEY = 'searchDb';
+const KEY = 'searchDb'
 
-const gSearches = storageService.load(KEY) || {};
+const gSearches = storageService.load(KEY) || {}
 
 export const mapService = {
-  initMap,
-  addMarker,
-  panTo,
-  getMap,
-  getSearchPosition,
-  getCityFromPos,
-};
+    initMap,
+    addMarker,
+    panTo,
+    getMap,
+    getSearchPosition,
+    getCityFromPos
+}
 
 var gMap;
 
 // window.mapService = mapService;
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
-  console.log('InitMap');
-  return _connectGoogleApi().then(() => {
-    console.log('google available');
-    gMap = new google.maps.Map(document.querySelector('#map'), {
-      center: {
-        lat,
-        lng,
-      },
-      zoom: 15,
-    });
-    // console.log('Map!', gMap);
-  });
+    console.log('InitMap');
+    return _connectGoogleApi()
+        .then(() => {
+            console.log('google available');
+            gMap = new google.maps.Map(
+                document.querySelector('#map'), {
+                    center: {
+                        lat,
+                        lng
+                    },
+                    zoom: 15                })
+            // console.log('Map!', gMap);
+        })
+        
 }
+
 
 function getMap() {
-  return gMap;
-}
+    return gMap
+};
 
 function addMarker(loc) {
-  var marker = new google.maps.Marker({
-    position: loc,
-    map: gMap,
-    title: 'Hello World!',
-  });
-  return marker;
+    var marker = new google.maps.Marker({
+        position: loc,
+        map: gMap,
+        title: 'Hello World!',
+    });
+    return marker;
 }
 
 function panTo(lat, lng) {
-  var laLatLng = new google.maps.LatLng(lat, lng);
-  gMap.panTo(laLatLng);
-  return Promise.resolve(getCityFromPos(lat, lng));
+    var laLatLng = new google.maps.LatLng(lat, lng);
+    gMap.panTo(laLatLng);
+    return Promise.resolve(getCityFromPos(lat,lng))
 }
 
 function _connectGoogleApi() {
-  if (window.google) return Promise.resolve();
-  var elGoogleApi = document.createElement('script');
-  elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
-  elGoogleApi.async = true;
-  document.body.append(elGoogleApi);
+    if (window.google) return Promise.resolve();
+    var elGoogleApi = document.createElement('script');
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+    elGoogleApi.async = true;
+    document.body.append(elGoogleApi);
 
-  return new Promise((resolve, reject) => {
-    elGoogleApi.onload = resolve;
-    elGoogleApi.onerror = () => reject('Google script failed to load');
-  });
-}
-
-function getSearchPosition(query) {
-  if (gSearches[query])
-    return Promise.resolve(gSearches[query].results[0].geometry.location);
-  console.log('getting from api');
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}k&key=${API_KEY}`;
-  return axios
-    .get(url)
-    .then(res => res.data)
-    .then(data => {
-      gSearches[query] = data;
-      storageService.save(KEY, gSearches);
-      const pos = data.results[0].geometry.location;
-      return pos;
+    return new Promise((resolve, reject) => {
+        elGoogleApi.onload = resolve;
+        elGoogleApi.onerror = () => reject('Google script failed to load');
     });
 }
 
-function getCityFromPos(lat, lng) {
-  // debugger
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${API_KEY}`;
-  console.log(lat, lng);
-  return axios
-    .get(url)
-    .then(res => res.data)
-    .then(({ results }) => results[0].address_components[2].long_name);
+function getSearchPosition(query) {
+    if (gSearches[query]) return Promise.resolve(gSearches[query].results[0].geometry.location)
+    console.log('getting from api')
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}k&key=${API_KEY}`
+    return axios.get(url)
+        .then(res => res.data)
+        .then((data) => {
+            gSearches[query] = data;
+            storageService.save(KEY, gSearches)
+            const pos = data.results[0].geometry.location
+            return pos
+        })
+
+}
+
+function getCityFromPos(lat,lng) {
+    // debugger
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${API_KEY}`
+    console.log(lat,lng)
+    return axios.get(url)
+        .then(res => res.data) 
+        .then(({results}) =>  results[0].address_components[2].long_name)
 }
--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -7,7 +7,7 @@ const KEY_API = '7401219c1d7f9d5d5648e6876684795c';
 function getWeather(lat, lng) {
   return axios
     .get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=${KEY_API}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=${KEY_API}`
     )
     .then(res => res.data)
     .then(data => {
